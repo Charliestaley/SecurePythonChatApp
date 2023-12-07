25a2fa84abf574aa12bc777a79dbfe010a1bcb56
Charliestaley <122784781+Charliestaley@users.noreply.github.com>
2023-12-07 10:09:41 +0000

    Add files via upload

---
+++ repos/client_2/client_2/client_2.py
@@ -0,0 +1,74 @@
+import decimal
+import socket
+import random
+from cryptography.fernet import Fernet
+from threading import Thread
+from datetime import datetime
+from colorama import Fore, init, Back
+
+# init colors
+init()
+
+# set the available colors
+colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX, 
+    Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX, 
+    Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX, 
+    Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW
+]
+
+# choose a random color for the client
+client_color = random.choice(colors)
+
+# server's IP address
+# if the server is not on this machine, 
+# put the private (network) IP address (e.g 192.168.1.2)
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 5002 # server's port
+separator_token = "<SEP>" # we will use this to separate the client name & message
+
+# initialize TCP socket
+s = socket.socket()
+print(f"[*] Connecting to {SERVER_HOST}:{SERVER_PORT}...")
+# connect to the server
+s.connect((SERVER_HOST, SERVER_PORT))
+print("[+] Connected.")
+
+# prompt the client for a name
+name = input("Enter your name: ")
+
+def listen_for_messages():
+    while True:
+        message = s.recv(1024).decode()
+        print("\n" + message)
+
+# make a thread that listens for messages to this client & print them
+t = Thread(target=listen_for_messages)
+# make the thread daemon so it ends whenever the main thread ends
+t.daemon = True
+# start the thread
+t.start()
+
+while True:
+    # input message we want to send to the server
+    to_send =  input()
+    # add the name & the color of the sender
+    to_send = f"{client_color}{name}{separator_token}{to_send}{Fore.RESET}"
+    # encrypt below the string
+    # use fernet to generate the key
+    key = Fernet.generate_key()
+    # instance the fernet class with the key
+    fernet = Fernet(key)
+    # use the Fernet class to encrypt
+    # string must be encoded to byte string before encryption
+    encMessage = fernet.encrypt(to_send.encode())
+    # decrypt the encrypted string with the Fernet key that was used to encrypt
+    decMessage = fernet.decrypt(encMessage.decode())
+    # a way to exit the program
+    if to_send.lower() == 'q':
+        break
+    # finally, send the message
+    s.send(encMessage)
+    s.send(decMessage)
+    
+# close the socket
+s.close()
